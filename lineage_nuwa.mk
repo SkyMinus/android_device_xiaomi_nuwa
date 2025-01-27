0cf617179da215e1ea3f2c61d3cfeaaf49d0aa6d
SkyMinus <minus_rav@qq.com>
2025-01-27 17:18:34 +0800

    nuwa: Initial rebrand
    Change-Id: Ia556aa968cb0f9d1dc6fc4ffa067b1f67844106c
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- lineage_nuwa.mk
+++ lineage_nuwa.mk
@@ -9,16 +9,16 @@ $(call inherit-product, build/make/target/product/full_base_telephony.mk)
 $(call inherit-product, build/make/target/product/core_64_bit_only.mk)
 
 # Inherit device-specific configurations
-$(call inherit-product, device/xiaomi/socrates/device.mk)
+$(call inherit-product, device/xiaomi/nuwa/device.mk)
 
 # Inherit LineageOS configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_socrates
-PRODUCT_DEVICE := socrates
-PRODUCT_MODEL := 22127RK46C
-PRODUCT_BRAND := Redmi
+PRODUCT_NAME := lineage_nuwa
+PRODUCT_DEVICE := nuwa
+PRODUCT_MODEL := 2210132C
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Redmi/socrates/socrates:15/AQ3A.240912.001/OS2.0.3.0.VMKCNXM:user/release-keys
+    BuildFingerprint=Redmi/nuwa/nuwa:15/AQ3A.240912.001/OS2.0.3.0.VMKCNXM:user/release-keys
